95b14322a1ee33451739470510042d669da10e47
Daan4 <daan_lubbers@hotmail.com>
2019-01-17 23:28:25 +0100

    pigpio mock library for testing on windows

---
+++ pigpio/__init__.py
@@ -0,0 +1,12 @@
+OUTPUT = None
+
+
+class pi:
+    def set_mode(self, *args, **kwargs):
+        pass
+
+    def write(self, *args, **kwargs):
+        pass
+
+    def callback(self, *args, **kwargs):
+        pass
