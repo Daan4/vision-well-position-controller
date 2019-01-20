71b9eafda5f1fab86c184ebaa712afbfd1792251
Daan4 <daan_lubbers@hotmail.com>
2019-01-20 20:59:16 +0100

    updated vision algorithm to work with new hardware setup

--- well_position_evaluators.py
+++ well_position_evaluators.py
@@ -36,6 +36,7 @@ class HoughTransformEvaluator(WellPositionEvaluator):
         self.centroid = None
         self.img_width, self.img_height = resolution
         if self.debug:
+            # todo if using this use unique window names
             cv2.namedWindow('Blur', cv2.WINDOW_NORMAL)
             cv2.resizeWindow('Blur', self.img_width, self.img_height)
             cv2.moveWindow('Blur', 50, 100)
@@ -125,7 +126,7 @@ class HoughTransformEvaluator(WellPositionEvaluator):
                     cv2.imshow('Result', original)
             # assume circle at index 0 is the best match
             self.centroid = (circles[0, 0, 0], circles[0, 0, 1])
-            offset = tuple(np.subtract(target, self.centroid))
+            offset = tuple(np.subtract(self.centroid, target))
         return offset
 
 
@@ -139,44 +140,28 @@ class WellBottomFeaturesEvaluator(WellPositionEvaluator):
         self.debug = debug
         self.centroid = None
         self.img_width, self.img_height = resolution
-        if self.debug:
-            cv2.namedWindow('Blur', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Blur', self.img_width, self.img_height)
-            cv2.moveWindow('Blur', 50, 100)
-
-            cv2.namedWindow('Gamma', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Gamma', self.img_width, self.img_height)
-            cv2.moveWindow('Gamma', 460, 100)
-
-            cv2.namedWindow('Threshold', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Threshold', self.img_width, self.img_height)
-            cv2.moveWindow('Threshold', 870, 100)
-
-            cv2.namedWindow('Morphology', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Morphology', self.img_width, self.img_height)
-            cv2.moveWindow('Morphology', 1280, 100)
-
-            cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Result', self.img_width, self.img_height)
-            cv2.moveWindow('Result', 50, 500)
 
         # Evaluation function parameters, scaled by image width if needed
         # parameters were originally determined for resolution of 410x308
         # blur
         self.blur_kernelsize = (25, 25)  # Has to be a square (for c implementation)
-        self.blur_kernelsize = tuple(np.multiply(self.blur_kernelsize, self.img_width / 410).astype(int))
-        self.blur_sigma = 100
+        #self.blur_kernelsize = tuple(np.multiply(self.blur_kernelsize, self.img_width / 410 + 0.5).astype(int))
+        self.blur_sigma = 1
+        
+        # manual threshold
+        self.threshold = 30
 
         # gamma
-        self.c = 2
-        self.gamma = 6
+        self.c = 0.5
+        self.gamma = 8
 
         # morphology
-        self.open_kernelsize = (49, 49)  # Has to be a square (for c implementation)
-        self.open_kernelsize = tuple(np.multiply(self.open_kernelsize, self.img_width / 410).astype(int))
+        self.open_kernelsize = (10, 10)  # Has to be a square (for c implementation)
+        self.close_kernelsize = (10, 10)
 
         # classification
         self.metric_threshold = 0.8
+        self.area_threshold = 10000
 
     def evaluate(self, img, target=(0, 0), benchmarking=False):
         """ Finds the position error by finding the well bottom centroid.
@@ -196,9 +181,36 @@ class WellBottomFeaturesEvaluator(WellPositionEvaluator):
             cols = img.shape[1]
             rows = img.shape[0]
             data = list(img.flat)
+            # todo update c implementation
             return wormvision.WBFE_evaluate(data, cols, rows, target, self.blur_kernelsize[0], self.blur_sigma, self.c, self.gamma,
                                             self.open_kernelsize[0], self.metric_threshold)
         else:
+            # recreate windows
+            cv2.namedWindow('Blur', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Blur', int(self.img_width/2+0.5), int(self.img_height/2+0.5))
+            cv2.moveWindow('Blur', 50, 100)
+
+            cv2.namedWindow('Gamma', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Gamma', int(self.img_width/2+0.5), int(self.img_height/2+0.5))
+            cv2.moveWindow('Gamma', 460, 100)
+
+            cv2.namedWindow('Threshold', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Threshold', int(self.img_width/2+0.5), int(self.img_height/2+0.5))
+            cv2.moveWindow('Threshold', 870, 100)
+
+            cv2.namedWindow('Scores', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Scores', int(self.img_width/2+0.5), int(self.img_height/2+0.5))
+            cv2.moveWindow('Scores', 1280, 100)
+
+            # cv2.namedWindow('Morphology', cv2.WINDOW_NORMAL)
+            # cv2.resizeWindow('Morphology', self.img_width, self.img_height)
+            # cv2.moveWindow('Morphology', 1280, 100)
+
+            cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Result', int(self.img_width/2+0.5), int(self.img_height/2+0.5))
+            cv2.moveWindow('Result', 50, 500)
+            
+            
             if benchmarking:
                 self.debug = False # dont show live images when benchmarking
             # use opencv library and show live images
@@ -209,7 +221,7 @@ class WellBottomFeaturesEvaluator(WellPositionEvaluator):
             # Gaussian filter
             img = cv2.GaussianBlur(img, self.blur_kernelsize, self.blur_sigma)
             if self.debug:
-                cv2.imshow('Blur', img)
+                cv2.imshow('Blur', cv2.resize(img, (int(self.img_width/2+0.5), int(self.img_height/2+0.5))))
 
             # Auto contrast
             _min = np.min(img)
@@ -225,33 +237,78 @@ class WellBottomFeaturesEvaluator(WellPositionEvaluator):
             img = np.multiply(img, self.c * 255)
             np.putmask(img, img > 255, 255)
             img = img.astype(np.uint8)
-            if self.debug:
-                cv2.imshow('Gamma', img)
+            
 
+            if self.debug:
+                cv2.imshow('Gamma', cv2.resize(img, (int(self.img_width/2+0.5), int(self.img_height/2+0.5))))
+                
             # Otsu threshold
-            _, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+            #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+            # For some reason img is not 8uc1 before, even though i cast to uint8 and cvt to gray initially???
+            #_, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+            #if self.debug:
+                #cv2.imshow('Threshold', img)
+                
+            # manual threshold
+            _, img = cv2.threshold(img, self.threshold, 255, cv2.THRESH_BINARY)
+            # close to fill small holes
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.close_kernelsize)
+            img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
             if self.debug:
                 cv2.imshow('Threshold', img)
 
             # Morphology
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.open_kernelsize)
-            img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-            if self.debug:
-                cv2.imshow('Morphology', img)
+            # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.open_kernelsize)
+            # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+            # if self.debug:
+            #     cv2.imshow('Morphology', img)
 
-            # Keep only the well bottom region
-            # By looking at region features
-            # step 1: Keep only blobs that score above roundness threshold
-            # step 2: Keep largest blob remaining after step 1
-            largest_area_above_threshold = -1
+            # # Keep only the well bottom region
+            # # By looking at region features
+            # # step 1: Keep only blobs that score above roundness threshold
+            # # step 2: Keep largest blob remaining after step 1
+            # largest_area_above_threshold = -1
+            # best_match = None
+            # im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # for c in contours:
+            #     area = cv2.contourArea(c)
+            #     perimeter = cv2.arcLength(c, True)
+            #     metric = 4 * np.pi * area / perimeter ** 2
+            #     if metric > self.metric_threshold and area > largest_area_above_threshold:
+            #         largest_area_above_threshold = area
+            #         best_match = c
+
+            # Select best match blob by looking at the mean score for
+            # roundness and eccentricity, lower is better
             best_match = None
+            best_score = 1.1
+            area_threshold = self.area_threshold
             im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            if self.debug:
+                im3 = img.copy()  # Copy to overlay results in
+                im3 = cv2.cvtColor(im3, cv2.COLOR_GRAY2BGR)
             for c in contours:
                 area = cv2.contourArea(c)
+                if area < area_threshold:
+                    continue
                 perimeter = cv2.arcLength(c, True)
-                metric = 4 * np.pi * area / perimeter ** 2
-                if metric > self.metric_threshold and area > largest_area_above_threshold:
-                    largest_area_above_threshold = area
+                roundness = 4 * np.pi * area / perimeter ** 2
+
+                # Calculate eccentricity from central moments as such:
+                # e = ((mu20 - mu02)^2 - 4*mu11^2)/(mu20+mu02)^2
+                m = cv2.moments(c, True)
+                eccentricity = ((m['m20'] - m['m02']) ** 2 - 4 * m['m11'] ** 2) / (m['m20'] + m['m02']) ** 2
+                score = (1 - roundness + eccentricity) / 2
+
+                if self.debug:
+                    # Overlay scores on image in debug mode
+                    cX = int(m["m10"] / m["m00"] + 0.5)
+                    cY = int(m["m01"] / m["m00"] + 0.5)
+                    cv2.putText(im3, '{0:.3f}'.format(score), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    cv2.imshow('Scores', im3)
+
+                if score < best_score:
+                    best_score = score
                     best_match = c
 
             # calculate centroid for best match blob
@@ -262,17 +319,19 @@ class WellBottomFeaturesEvaluator(WellPositionEvaluator):
                     cv2.imshow('Result', original)
             else:
                 M = cv2.moments(best_match)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
+                cX = int(M["m10"] / M["m00"] + 0.5)
+                cY = int(M["m01"] / M["m00"] + 0.5)
                 self.centroid = (cX, cY)
-                offset = tuple(np.subtract(target, self.centroid))
+                offset = tuple(np.subtract(self.centroid, target))
                 if self.debug:
                     # Overlay results on source image and display them
                     cv2.circle(original, target, 5, 0, 1)
                     cv2.circle(original, self.centroid, 4, 0, 5)
-                    cv2.imshow('Result', original)
+                    cv2.imshow('Result', cv2.resize(original, (int(self.img_width/2+0.5), int(self.img_height/2+0.5))))
             if benchmarking:
                 self.debug = True
+            
+            
             return offset
 
 
@@ -326,11 +385,13 @@ def test_wellbottomfeaturesevaluator():
         cv2.waitKey(0)
     else:
         # single run to compare c vs opencv result
-        imgpath = 'D:\\Libraries\\Documents\\svn\\EVD_PROJ\\99-0. Overig\\05. Images of C. Elegans (11-10-2018)\\all_downscaled\\manualControl_v0.2.py_1538674924133_downscaled.png'
+        imgpath = 'C:\\Users\\Daan\\Documents\\EVD_PROJ\\99-0. Overig\\05. Images of C. Elegans (11-10-2018)\\all_downscaled\\manualControl_v0.2.py_1538674924133_downscaled.png'
         x = WellBottomFeaturesEvaluator((410, 308), True)
-        print('opencv: {}'.format(x.evaluate(cv2.imread(imgpath, cv2.CV_8UC1), (227, 144), True)))
-        x.debug = False
-        print('c: {}'.format(x.evaluate(cv2.imread(imgpath, cv2.CV_8UC1), (227, 144), False)))
+        img = cv2.imread(imgpath, cv2.CV_8UC1)
+        print('opencv: {}'.format(x.evaluate(img, (227, 144), False)))
+        cv2.waitKey(0)
+        # x.debug = False
+        # print('c: {}'.format(x.evaluate(cv2.imread(imgpath, cv2.CV_8UC1), (227, 144), False)))
 
 
 def test_houghtransformevaluator():
