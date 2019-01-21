d6f3e8158d15eb15a09cba33f0d2ab4f2af89e68
Daan4 <daan_lubbers@hotmail.com>
2019-01-21 21:32:46 +0100

    clean up and improve comments

--- camera.py
+++ camera.py
@@ -1,27 +1,31 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
 import numpy as np
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 from picamera import PiCamera
-from picamera.array import PiRGBArray, PiYUVArray
+from picamera.array import PiRGBArray
 import time
 
-## PiVideoStream class streams camera images to a pyqt signal in a separate thread.
-#
+
 class PiVideoStream(QThread):
-    name = "PiVideoStream"
-    ## Logging message signal
-    sigMsg = pyqtSignal(str)  # Message signal
-    ## Image signal as numpy array
-    ready = pyqtSignal(np.ndarray)
-    pause = False
-    
-    ## The constructor.
-    def __init__(self, resolution=(640,480), framerate=24, imgformat='bgr', effect='none', use_video_port=False):
-        super().__init__()        
+    def __init__(self, resolution=(640, 480), framerate=24, imgformat='bgr', effect='none', use_video_port=False):
+        """
+
+        Args:
+            resolution: frame resolution
+            framerate: frames per second
+            imgformat: image format (defaults to bgr for opencv compatibility)
+            effect: camera effect
+            use_video_port: ???
+        """
+        super().__init__()
         self.camera = PiCamera()
-        self.initCamera(resolution, framerate, imgformat, effect, use_video_port)
-        self.sigMsg.emit(self.name + ": opened.")
+        self.frame = None
+        self.rawCapture = None
+        self.stream = None
+        self.init_camera(resolution, framerate, imgformat, effect, use_video_port)
+        self.sig_msg = pyqtSignal(str)  # logging message signal
+        self.ready = pyqtSignal(np.ndarray)  # Image signal as numpy array
+        self.pause = False
+        self.sig_msg.emit(self.__class__.__name__ + ": opened.")
 
     def __del__(self):
         self.wait()
@@ -30,56 +34,40 @@ class PiVideoStream(QThread):
         try:
             for f in self.stream:
                 self.rawCapture.truncate(0)  # clear the stream in preparation for the next frame
-                if (self.pause == True):
-                    self.sigMsg.emit(self.name + ": paused.")
-                    break  # return from thread is needed
+                if self.pause:
+                    self.sig_msg.emit(self.__class__.__name__ + ": paused.")
+                    break   # return from thread is needed
                 else:
                     self.frame = f.array  # grab the frame from the stream
                     self.ready.emit(self.frame)
-##                    self.sigMsg.emit(self.name + ": frame captured.")
         except Exception as err:
-            self.sigMsg.emit(self.name + ": error running thread.")
-            pass
+            self.sig_msg.emit(self.__class__.__name__ + ": error running thread. " + str(err))
         finally:
-            self.sigMsg.emit(self.name + ": quit.")
+            self.sig_msg.emit(self.__class__.__name__ + ": quit.")
 
-    def initCamera(self, resolution=(640,480), framerate=24, imgformat='bgr', effect='none', use_video_port=False):
-        self.sigMsg.emit(self.name + "Init: resolution = " + str(resolution))
+    def init_camera(self, resolution=(640, 480), framerate=24, imgformat='bgr', effect='none', use_video_port=False):
+        self.sig_msg.emit(self.__class__.__name__ + "Init: resolution = " + str(resolution))
         self.camera.resolution = resolution
         self.camera.image_effect = effect
-        self.camera.iso = 60 # should force unity analog gain
-##        self.camera.color_effects = (128,128) # return monochrome image
-        # dunno if setting awb mode manually is really useful
-##        self.camera.awb_mode = 'off'
-##        self.camera.awb_gains = 5.0
-##        self.camera.meter_mode = 'average'
-##        self.camera.exposure_mode = 'auto'  # 'sports' to reduce motion blur, 'off'after init to freeze settings
+        self.camera.iso = 60
         self.camera.framerate = framerate
         self.rawCapture = PiRGBArray(self.camera, size=self.camera.resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture, imgformat, use_video_port)
         self.frame = None
-        time.sleep(2)  
+        time.sleep(2)
 
     @pyqtSlot()
     def stop(self):
         self.pause = True
         self.wait()
-##        self.rawCapture.close()
-##        self.camera.close()
         self.quit()
-        self.sigMsg.emit(self.name + ": closed.")
+        self.sig_msg.emit(self.__class__.__name__ + ": closed.")
 
     @pyqtSlot()
-    def changeCameraSettings(self, resolution=(640,480), framerate=24, format="bgr", effect='none', use_video_port=False):
+    def change_camera_settings(self, resolution=(640, 480), framerate=24, _format="bgr", effect='none',
+                               use_video_port=False):
         self.pause = True
         self.wait()
-        self.initCamera(resolution, framerate, format, effect, use_video_port)
+        self.init_camera(resolution, framerate, _format, effect, use_video_port)
         self.pause = False
         self.start()  # restart thread
-
-
-if __name__ == "__main__":
-    vs = PiVideoStream(effect='denoise')
-    vs.start()
-    #vs.ready.connect(mainWindow.kickTimer)  # Measure time delay
-    #vs.ready.connect(imgEnhancer.imgUpdate, type=Qt.QueuedConnection)  # Connect video/image stream to processing
